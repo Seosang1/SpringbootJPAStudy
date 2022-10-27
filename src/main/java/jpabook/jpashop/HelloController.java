9314cb90ab9eb526fbf9b3a09433f407c71b64ee
Seosang1 <85483691+Seosang1@users.noreply.github.com>
2022-10-27 20:00:00 +0900

    JPA test commit

--- src/main/java/jpabook/jpashop/HelloController.java
+++ src/main/java/jpabook/jpashop/HelloController.java
@@ -1,17 +1,25 @@
 package jpabook.jpashop;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter  @Setter
+@Controller
 public class HelloController {
-
-	@GetMapping("hello")
+	/*
+	 * @RequestMapping("/")
+	 * 
+	 * @ResponseBody public String home() { System.out.println("home in"); return
+	 * "index"; }
+	 */
+	
+	@GetMapping("/hello")
 	public String hello(Model model) {
+		System.out.println("hello in ");
 		model.addAttribute("data","hello!");
+		System.out.println("model >>> "+ model.toString());
 		return "hello";
 	}
 }
---
+++ src/main/java/jpabook/jpashop/MemberRepo.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepo {
+	
+	@PersistenceContext
+	private EntityManager em;
+	
+	public Long save(Member member) {
+		em.persist(member);
+		return member.getId();
+	}
+	
+	// 멤버 찾기
+	public Member find(Long id) {
+		return em.find(Member.class, id);
+	}
+
+}
